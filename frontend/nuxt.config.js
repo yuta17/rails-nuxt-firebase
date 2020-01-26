244a81c2dce954796411c678f174cd19071cf82e
Hassan <usgitan@gmail.com>
2020-01-26 17:11:20 +0900

    Generate sorce to public of rails

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   mode: 'universal',
+  geneerate: {
+    dir: '../public'
+  },
   /*
    ** Headers of the page
    */
@@ -55,6 +58,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) { }
   }
 }
